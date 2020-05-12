3d3f8a72ed9da493feff137b6f1251c5c0b620fc
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-05-12 19:49:27 -0300

    has_many registrations in player

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,10 +1,4 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :genre, :age, :registrations
-
-  def registrations
-    id = object.id
-    record = Player.find(id).registrations
-    resource = ActiveModelSerializers::SerializableResource.new(record, {}).to_json
-    return resource
-  end
+  attributes :id, :name, :genre, :age
+  has_many :registrations
 end
