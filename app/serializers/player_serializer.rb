2c572f5e717e3299f5976d71d170e7e040eeb81b
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-05-13 22:10:23 -0300

    has_many registrations in player

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,4 +1,4 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :name, :genre, :age
-  has_many :registrations
+  has_many :registrations, serializer: RegistrationSerializer
 end
