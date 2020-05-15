c42894b9b9d5414c4e383597c354d866ab6783eb
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-05-15 17:27:34 -0300

    Add score for players

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,4 +1,13 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :genre, :age
+  attributes :id, :name, :genre, :age, :score
   has_many :registrations
+
+  def score
+    total = 0
+    object.registrations.each do |registration|
+      total += registration.position.score
+    end
+    return total
+  end
+
 end
