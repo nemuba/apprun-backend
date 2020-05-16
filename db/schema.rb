1cdfcf906419efcf91f01dac3c6de2894e38a547
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-05-16 09:29:19 -0300

    Add column in user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_192124) do
+ActiveRecord::Schema.define(version: 2020_05_16_122826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_05_15_192124) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "race_modalities", "modalities"
