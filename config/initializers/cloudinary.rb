dc7532b625d431a6b244a1331a5afcd93a471934
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-05-17 14:17:01 -0300

    Add cloudinary config

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,3 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'ojeda'
-  config.api_key = '635492153654223'
-  config.api_secret = 'iCXAcHyO-KulN78TgZ_MeOT233c'
-  config.enhance_image_tag = true
-  config.static_file_support = false
+  config.secure = true
 end
